93e5cdad66138f697bc5748f00b72e97a2045d35
ReimuYk <513639514@qq.com>
2018-10-29 12:44:58 +0800

    binary operation

--- morphologyoperation.py
+++ morphologyoperation.py
@@ -67,9 +67,69 @@ def gray_erosion(ori_img,se):
     h=se_height-1
     return array(ret_img)[h:-h,w:-w]
 
+# ori_img & se : array ( binary img ),0==0,!0==1
+# center (x,y) or [x,y]
+# ret_val: ret_img ( binary img )
+def bi_dilation(ori_img,se,center=None):
+    img_height = len(ori_img)
+    img_width = len(ori_img[0])
+    se_height = len(se)
+    se_width = len(se[0])
+    # init center
+    if center==None:
+        center = [int(se_height/2),int(se_width/2)]
+    # init ret_img
+    ret_height = img_height+se_height-1
+    ret_width = img_width+se_width-1
+    ret_img = [[0]*ret_width for k in range(ret_height)]
+    # create ret_img
+    for h in range(img_height):
+        for w in range(img_width):
+            if ori_img[h][w]!=0:
+                for se_h in range(se_height):
+                    for se_w in range(se_width):
+                        if se[se_h][se_w]!=0:
+                            ret_img[h+se_h][w+se_w]=1
+    return array(ret_img)
+
+
+# ori_img & se : array ( binary img ),0==0,!0==1
+# center (x,y) or [x,y]
+# ret_val: ret_img ( binary img )
+def bi_erosion(ori_img,se,center=None):
+    img_height = len(ori_img)
+    img_width = len(ori_img[0])
+    se_height = len(se)
+    se_width = len(se[0])
+    # init center
+    if center==None:
+        center = [int(se_height/2),int(se_width/2)]
+    # init ret_img
+    ret_height = img_height-se_height+1
+    ret_width = img_width-se_width+1
+    ret_img = [[0]*ret_width for k in range(ret_height)]
+    # create ret_img
+    for h in range(ret_height):
+        for w in range(ret_width):
+            flag = True
+            for se_h in range(se_height):
+                for se_w in range(se_width):
+                    th_h = int(h+se_h)
+                    th_w = int(w+se_w)
+                    if se[se_h][se_w]!=0 and ori_img[th_h][th_w]==0:
+                        flag=False
+            if flag:
+                ret_img[h][w]=1
+    return array(ret_img)
+            
+
 ori_img=array([[0,1,0,0],[1,1,1,1],[0,1,1,0],[0,0,0,1]])
-se=array([[0,1,0],[1,1,0]])
-ret=gray_dilation(gray_erosion(ori_img,se),se)
+se=array([[0,1],[1,1]])
+##ret=bi_dilation(ori_img,se)
+ret1=bi_erosion(ori_img,se)
+ret=bi_dilation(ret1,se)
+
+
 
 ##img2 = Image.fromarray(ori_img)
 ##
